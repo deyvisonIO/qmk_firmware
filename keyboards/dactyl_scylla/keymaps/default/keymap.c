e7f8ad73091bb89e002b759d449160c595d144a9
Deyvison Iury <iurydeyvison@gmail.com>
2024-09-09 13:39:10 -0300

    Add games layer

--- keyboards/dactyl_scylla/keymaps/default/keymap.c
+++ keyboards/dactyl_scylla/keymaps/default/keymap.c
@@ -3,9 +3,12 @@
 #define _QWERTY 0
 #define _SYMBOLS 1
 #define _NAVIGATION 2
+#define _GAMES 2
+
 
 #define NAVIGATION LT(_NAVIGATION, KC_BSPC)
 #define SYMBOLS LT(_SYMBOLS, KC_SPC)
+#define GAMES TG(_GAMES)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
@@ -33,6 +36,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT, KC_PLUS,
         _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,    KC_PIPE,
                               _______, _______, _______,                        _______, _______, _______,
-                                       _______, _______,                        _______, _______
+                                       _______, _______,                        _______, GAMES
     )
+
+
+    [_GAMES] = LAYOUT_5x6(
+        KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8   , KC_9   , KC_0   , KC_QUOTE,
+        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I   , KC_O   , KC_P   , KC_MINS,
+        KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K   , KC_L   , KC_SCLN, KC_EQL,
+        KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_BSLS,
+                            KC_SPC, NAVIGATION, KC_LSFT,                         KC_RSFT, KC_ENT, KC_SPC,
+                                       KC_LALT, KC_LCTL,                         KC_RCTL, KC_LGUI
+    )
+
+
 };
